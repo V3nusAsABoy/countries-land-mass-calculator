8ffa76d41e070e5d66da0985522f7e9f80318549
Andrew Abdulaziz <andrewalloniealt@gmail.com>
2025-06-03 11:49:17 -0400

    add country function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  const addCountry = () => {
+    setCountries((c) => [...c,""]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {countries.map((country) => {
+        return <p>C</p>
+      })}
+      <button onClick = {addCountry}>Add</button>
+    </>
   );
 }
 
