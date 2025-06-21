703707b956b31f38235213f594a2cf07091beecd
Andrew Abdulaziz <andrewalloniealt@gmail.com>
2025-06-21 11:33:36 -0400

    Made header for guide

--- src/Guide.js
+++ src/Guide.js
@@ -3,6 +3,11 @@ import './Countries.css';
 const Guide = () => {
     return(
         <>
+            <div id = "body">
+                <div id = "header">
+                    <h1>Guide</h1>
+                </div>
+            </div>
         </>
     )
 }
