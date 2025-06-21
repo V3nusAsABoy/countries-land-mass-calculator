d95af0404009d81ead3705eb129ce809a255f1ac
Andrew Abdulaziz <andrewalloniealt@gmail.com>
2025-06-21 12:30:20 -0400

    switched back to app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import Guide from './Guide.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Guide />
+    <App />
   </React.StrictMode>
 );
